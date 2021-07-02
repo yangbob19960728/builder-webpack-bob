f0dfbb645056228d0e55c6c26cf650cfe385819e
BobYang <f3612453@gmail.com>
2021-07-02 23:20:25 +0800

    修正路徑，因為CI沒有這個絕對路徑

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -5,7 +5,7 @@ const baseConfig = require('../../lib/webpack.base');
 describe('webpack.base.js test case', () => {
     console.log(baseConfig);
     it('entry', () => {
-        assert.equal(baseConfig.entry.index, 'D:/webpack/builder-webpack/test/smoke/template/src/index/index.js');
-        assert.equal(baseConfig.entry.main, 'D:/webpack/builder-webpack/test/smoke/template/src/main/index.js');
+        assert.equal(baseConfig.entry.index.indexOf('/test/smoke/template/src/index/index.js') > -1, true);
+        assert.equal(baseConfig.entry.main.indexOf('/test/smoke/template/src/main/index.js') > -1, true);
     });
 });
